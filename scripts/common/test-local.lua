b0d72eaa7fcaf67d57f8fc108f495e724179df97
Mike Kristoffersen <mike@trout.software>
2025-01-15 14:48:53 +0000

    Updated sample config in the ubuntu folder

--- scripts/common/test-local.lua
+++ scripts/common/test-local.lua
@@ -1,18 +1,22 @@
 
 
---output_to_file = { file_name = 'my_output_file.txt' }
---output_to_pipe = { pipe_name = '/tmp/llpipe_17115325107265013479' }                                            
---output_to_pipe = { pipe_env = 'pipename' }
-output_to_stdout = {}
+serializer_txt = {}
+serializer_lorth = {}
+serializer_bill = { bill_secret_sequence = '001122334455667788' }
+--serializer_bill = { bill_secret_env = 'secret_bill'}
 
---log_bill = { output = 'output_to_pipe' }
---log_txt = { output = 'output_to_stdout' }
-log_lorth = { output = 'output_to_stdout' }
 
-alert_lioli = { logger = 'log_txt' }
+logger_file = { file_name = 'test.lorth',
+                serializer = 'serializer_lorth' }
+logger_null = { }
+logger_stdout = { serializer = 'serializer_txt' }
+logger_pipe = { serializer = 'serializer_bill',
+                pipe_name = "/tmp/my_pipe",
+                restart_interval_s = '60' }
 
-trout_netflow = { logger = 'log_txt',
-                   option_grouped_output = false }                   
+alert_lioli = { logger = 'log_file' }
+
+trout_netflow = { logger = 'log_file' }                   
 
 stream = {}
 stream_tcp = {}
