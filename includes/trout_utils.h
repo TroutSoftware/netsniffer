875a621f91dc25a32aa13d9e220968e4d3217138
Mike Kristoffersen <mike@trout.software>
2025-09-16 10:34:04 +0200

    Added missing file from previous commits

---
+++ includes/trout_utils.h
@@ -0,0 +1,28 @@
+#ifndef trout_utils_baffe25a
+#define trout_utils_baffe25a
+
+// Snort includes
+
+// System includes
+#include <random>
+
+// Local includes
+
+// Debug includes
+
+namespace Common {
+
+// Might be a pseudo-random on some systems
+class Random {
+  std::random_device rd;
+public:
+  // Returns random number between min and max (both min and max can be returned)
+  int random(int min, int max) {
+    assert(min<=max);
+    std::uniform_int_distribution<> distrib(min, max);
+    return distrib(rd);
+  }
+};
+
+} // namespace Common
+#endif // #ifndef trout_utils_baffe25a
