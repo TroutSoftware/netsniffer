0d4b4dc3d4bef104c707a255321eb0a474550f99
Mike Kristoffersen <mike@trout.software>
2026-02-04 13:54:05 +0100

    Fixed the Alpine launch script so it doesn't have the be launched from its folder

--- test_envr_alpn/bshell.sh
+++ test_envr_alpn/bshell.sh
@@ -1,8 +1,8 @@
+MY_HOME="$(cd "$(dirname "$0")" && pwd)"
 
-
-redo-ifchange envrc config-host install
-. ./envrc
-. ./build-lib.rc
+redo-ifchange $MY_HOME/envrc $MY_HOME/config-host $MY_HOME/install
+. $MY_HOME/envrc
+. $MY_HOME/build-lib.rc
 
 echo launching...
 
